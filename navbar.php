bb006e9e29e4453602912493bb19b252af601bbf
AlanPR27 <alan.placeresro@outlook.com>
2026-04-23 20:41:27 -0600

    agregue readme y pruebas de funcionalidad basica

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="index.php">GameStore 🎮</a>
+        <a class="navbar-brand" href="index.php">Tienda Videojuegos Alan</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
             <span class="navbar-toggler-icon"></span>
